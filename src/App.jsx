ba4bb5a2c25d6f217427adbd5c28a4c045620626
Ahmed <ahmed.masar1400@gmail.com>
2025-03-29 17:39:29 +0300

    fixed errre

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import Header from "./compontents/1-header/Header";
 import About from "./compontents/2-about/About";
 import Program from "./compontents/3-program/Program";
 import Service from "./compontents/4-service/Service";
-import Footer from "./compontents/6-footer/footer";
-
+import Footer from "./compontents/6-footer/Footer";
 function App() {
   return (
     <>
